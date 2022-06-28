3a3e5c7e37b961404c1037bb00f9ba2693675e66
Anshul Baliga <baligaanshul@gmail.com>
2022-06-28 20:38:38 +0530

    Finished step 5

--- lexi.c
+++ lexi.c
@@ -68,7 +68,7 @@ struct editorConfig E;
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 /*** terminal ***/
 
 // Terminal starts in canonical mode by default (Input sent only when enter is pressed)
@@ -239,6 +239,18 @@ int editorRowCxToRx(erow *row, int cursor_x)
   }
   return rx;
 } // converts a chars index into a render index
+
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
+} 
 
 void editorUpdateRow(erow *row)
 {
@@ -430,7 +442,7 @@ void editorSave()
 {
   if (E.filename == NULL)
   {
-    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
     if (E.filename == NULL)
     {
       editorSetStatusMessage("Save aborted");
@@ -457,6 +469,55 @@ void editorSave()
   }
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+/*** find ***/
+void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
+    return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
+  }
+  if (last_match == -1) direction = 1;
+  int current = last_match;
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    current += direction;
+    if (current == -1) current = E.numrows - 1;
+    else if (current == E.numrows) current = 0;
+    erow *row = &E.row[current];
+    char *match = strstr(row->render, query);
+    if (match) {
+      last_match = current;
+      E.cy = current;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+      break;
+    }
+  }
+}
+void editorFind() {
+  int saved_cx = E.cx;
+  int saved_cy = E.cy;
+  int saved_coloff = E.coloff;
+  int saved_rowoff = E.rowoff;
+  char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)",editorFindCallback);
+  if (query) {
+    free(query);
+  } else {
+    E.cx = saved_cx;
+    E.cy = saved_cy;
+    E.coloff = saved_coloff;
+    E.rowoff = saved_rowoff;
+  }
 }
 /*** append buffer ***/
 
@@ -626,8 +687,7 @@ void editorSetStatusMessage(const char *fmt, ...)
 
 /*** input ***/
 
-char *editorPrompt(char *prompt)
-{
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
   size_t bufsize = 128;
   char *buf = malloc(bufsize);
   size_t buflen = 0;
@@ -645,6 +705,7 @@ char *editorPrompt(char *prompt)
     else if (c == '\x1b')
     {
       editorSetStatusMessage("");
+      if (callback) callback(buf, c);
       free(buf);
       return NULL;
     }
@@ -653,6 +714,7 @@ char *editorPrompt(char *prompt)
       if (buflen != 0)
       {
         editorSetStatusMessage("");
+        if (callback) callback(buf, c);
         return buf;
       }
     }
@@ -666,6 +728,7 @@ char *editorPrompt(char *prompt)
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
+    if (callback) callback(buf, c);
   }
 }
 
@@ -751,6 +814,9 @@ void editorProcessKeypress() // to wait for a keypress and handles it
     if (E.cursor_y < E.numrows)
       E.cursor_x = E.row[E.cursor_y].size;
     break;
+  case CTRL_KEY('f'):
+      editorFind();
+      break;
   case BACKSPACE:
   case CTRL_KEY('h'):
   case DEL_KEY:
@@ -821,8 +887,7 @@ int main(int argc, char *argv[])
   {
     editorOpen(argv[1]);
   }
-
-  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-E = quit");
+  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
   while (1)
   {
     editorRefreshScreen(); //
